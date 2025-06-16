77864d9993811da616101b0d02c80703b7beb03f
TaiNHCE <TaiNHCE181569@fpt.edu.vn>
2025-06-16 21:43:53 +0700

    Update Product.java

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -4,18 +4,17 @@
  */
 package model;
 
-import java.sql.Date;
+import java.math.BigDecimal;
 
 /**
  *
- * @author HP
+ * @author HP - Gia Khiêm
  */
 public class Product {
-
     private int productId;
     private String productName;
     private String description;
-    private double price;
+    private BigDecimal  price;
     private int discount;
     private int stock;
     private String status;
@@ -25,18 +24,11 @@ public class Product {
     private boolean isFeatured;
     private boolean isBestSeller;
     private boolean isNew;
-    private Date createdAt;
-    private Date updateAt;
     private int warrantyPeriod;
     private boolean isActive;
+    private String imageUrl;
 
-    public Product() {
-    }
-
-    public Product(int productId, String productName, String description, double price, int discount,
-            int stock, String status, int supplierId, int categoryId, int brandId,
-            boolean isFeatured, boolean isBestSeller, boolean isNew,
-            Date createdAt, Date updateAt, int warrantyPeriod, boolean isActive) {
+    public Product(int productId, String productName, String description, BigDecimal price, int discount, int stock, String status, int supplierId, int categoryId, int brandId, boolean isFeatured, boolean isBestSeller, boolean isNew, int warrantyPeriod, boolean isActive, String imageUrl) {
         this.productId = productId;
         this.productName = productName;
         this.description = description;
@@ -50,145 +42,138 @@ public class Product {
         this.isFeatured = isFeatured;
         this.isBestSeller = isBestSeller;
         this.isNew = isNew;
-        this.createdAt = createdAt;
-        this.updateAt = updateAt;
         this.warrantyPeriod = warrantyPeriod;
         this.isActive = isActive;
+        this.imageUrl = imageUrl;
     }
 
     public int getProductId() {
         return productId;
     }
 
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
     public String getProductName() {
         return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
+    public BigDecimal getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     public int getDiscount() {
         return discount;
     }
 
-    public void setDiscount(int discount) {
-        this.discount = discount;
-    }
-
     public int getStock() {
         return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     public int getSupplierId() {
         return supplierId;
-    }
-
-    public void setSupplierId(int supplierId) {
-        this.supplierId = supplierId;
     }
 
     public int getCategoryId() {
         return categoryId;
     }
 
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
     public int getBrandId() {
         return brandId;
     }
 
-    public void setBrandId(int brandId) {
-        this.brandId = brandId;
-    }
-
-    public boolean isFeatured() {
+    public boolean isIsFeatured() {
         return isFeatured;
     }
 
-    public void setFeatured(boolean featured) {
-        isFeatured = featured;
-    }
-
-    public boolean isBestSeller() {
+    public boolean isIsBestSeller() {
         return isBestSeller;
     }
 
-    public void setBestSeller(boolean bestSeller) {
-        isBestSeller = bestSeller;
-    }
-
-    public boolean isNew() {
+    public boolean isIsNew() {
         return isNew;
-    }
-
-    public void setNew(boolean aNew) {
-        isNew = aNew;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
     }
 
     public int getWarrantyPeriod() {
         return warrantyPeriod;
     }
 
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
+    }
+
+    public void setIsFeatured(boolean isFeatured) {
+        this.isFeatured = isFeatured;
+    }
+
+    public void setIsBestSeller(boolean isBestSeller) {
+        this.isBestSeller = isBestSeller;
+    }
+
+    public void setIsNew(boolean isNew) {
+        this.isNew = isNew;
+    }
+
     public void setWarrantyPeriod(int warrantyPeriod) {
         this.warrantyPeriod = warrantyPeriod;
     }
 
-    public boolean isActive() {
-        return isActive;
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
     }
 
-    public void setActive(boolean active) {
-        isActive = active;
+    public String getImageUrl() {
+        return imageUrl;
     }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    
+    
 }
